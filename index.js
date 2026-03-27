03a7bad3665a9e9100abe82f52255d7f0872620e
SeanByr <seanbyrne123@outlook.ie>
2026-03-27 11:37:00 +0000

    added Desktop Capturer from docs

---
+++ index.js
@@ -0,0 +1,19 @@
+// main.js
+const { app, BrowserWindow, desktopCapturer, session } = require('electron')
+
+app.whenReady().then(() => {
+  const mainWindow = new BrowserWindow()
+
+  session.defaultSession.setDisplayMediaRequestHandler((request, callback) => {
+    desktopCapturer.getSources({ types: ['screen'] }).then((sources) => {
+      // Grant access to the first screen found.
+      callback({ video: sources[0], audio: 'loopback' })
+    })
+    // If true, use the system picker if available.
+    // Note: this is currently experimental. If the system picker
+    // is available, it will be used and the media request handler
+    // will not be invoked.
+  }, { useSystemPicker: true })
+
+  mainWindow.loadFile('index.html')
+})
